209620f8936c9f74be1901bedd2e5a5ff259686e
Sean Ng <sean.ng.cip@gmail.com>
2021-11-29 14:04:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const express = require("express");
 const PORT = process.env.PORT || 8007;
 const app = express();
+const fsP = require("fs").promises;
 
 // Don't worry about these 4 lines below
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.post("/people/homepage", (req, res) => {
   res.render("homepage");
 });
-app.get("/people/:id", (req, res) => {
+
+app.get("/", (req, res) => {
+  res.render("createcard")
+})
+
+// app.post("/", (req, res) => {
+//   res.render("homepage")
+// })
+
+// app.post("/create", (req, res) => {
+//   const user = req.body;
+//   fsP.readFile("database.json", "utf-8")
+//     .then((content) => JSON.parse(content))
+//     .then(jsonObj)
+// })
+
+app.post("/people/:id", (req, res) => {
+  const id = "54az3"; // Get ID from URL
+  fsP.readFile("database.json", "utf-8")
+    .then()// Find matching database from .json
+});
+
+app.post(`/people/:id`, (req, res) => {
   res.render("people");
 });
 
-app.get("/:id/photos", (req, res) => {
+app.post("/:id/photos", (req, res) => {
   const id = req.params.id;
 });
 
